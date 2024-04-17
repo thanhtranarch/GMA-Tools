3eb0a30d9f38003c8b91925fd08e8e8896137fc7
Tran Tien Thanh <134719719+thanhtranarch@users.noreply.github.com>
2024-04-17 16:48:23 +0700

    Update createplanview.py

--- createplanview.py
+++ createplanview.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Make Plans
 Create Plans from Room List
@@ -6,7 +7,7 @@ Author: Tran Tien Thanh
 """
 
 __author__ ="Tran Tien Thanh"
-__title__ = "Plan Views"
+__title__ = "Create Plan Views"
 
 from rpw import revit, DB, db
 
@@ -15,57 +16,42 @@ from Autodesk.Revit.DB import (
     View,
     FilteredElementCollector,
     BuiltInCategory,
-    ViewType
+    ViewType,
+    ViewFamilyType,
+    BoundingBoxXYZ
 )
 from Autodesk.Revit.UI import TaskDialog
 from pyrevit import forms
+from collections import namedtuple
+import re
 
 """--------------------------------------------------"""
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
+pattern=r'[^a-zA-Z0-9.]'
 """--------------------------------------------------"""
-rooms = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms).ToElements()
-new_views = []
-viewlist = FilteredElementCollector(doc).OfClass(View).WhereElementIsNotElementType().ToElements()
-vtemp = {}
-for v in viewlist:
-    if v.IsTemplate and 'master' not in v.Name.lower():
-        vtemp[v.Name]=v.Id.IntegerValue
-plan_templates=sorted([v.Name for v in viewlist if v.ViewType == ViewType.FloorPlan and v.Id.IntegerValue in vtemp.values()])
-RCP_templates=sorted([v.Name for v in viewlist if v.ViewType == ViewType.CeilingPlan and v.Id.IntegerValue in vtemp.values()])
-template_plan=None
-template_RCP=None
-list_rooms={}
-rooms_type = [("{} | #{}".format(room.LookupParameter("Name").AsString(), room.LookupParameter("Number").AsString())) for room in rooms]
-room_type_sorted = sorted(rooms_type, key=lambda x: x[0])  # Sort by room name
-plan_types = db.Collector(of_class='ViewFamilyType', is_type=True).get_elements(wrapped=True)
-plan_types_options = {t.name for t in plan_types
-                      if t.view_family.name in ('FloorPlan', 'CeilingPlan')}
+#Collect Room & Filter:
+room_obj        = namedtuple("room_obj",["name","number","type","obj"])
+room_collected  = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms).ToElements()
+room_list       = map(lambda r_obj: room_obj(r_obj.LookupParameter("Name").AsString(),r_obj.LookupParameter("Number").AsString(),r_obj.LookupParameter("Room Type").AsString(),r_obj),room_collected)
+room_obj_unit   = map(lambda r_obj: "UNIT TYPE: {} | #{} | {}".format(r_obj.type,r_obj.number,r_obj.name),filter(lambda r_obj: "UNIT" in r_obj.name.upper() and r_obj.type != None,room_list ))
+room_obj_common = map(lambda r_obj: "{} | #{}".format(r_obj.name,r_obj.number),filter(lambda r_obj: "UNIT" not in r_obj.name.upper(),room_list))
+room_general    = sorted(room_obj_common + room_obj_unit)
+
+#Collect Viewtemplate:
+view_temp       = namedtuple("view_temp",["name","obj"])
+view_collected  = FilteredElementCollector(doc).OfClass(View).WhereElementIsNotElementType().ToElements()
+view_temp_plan  = map(lambda v_obj: view_temp(v_obj.Name,v_obj), filter(lambda v_obj: v_obj.ViewType == ViewType.FloorPlan and v_obj.IsTemplate,view_collected))
+view_temp_RCP   = map(lambda v_obj: view_temp(v_obj.Name,v_obj), filter(lambda v_obj: v_obj.ViewType == ViewType.CeilingPlan and v_obj.IsTemplate,view_collected))
+
+#Collect Viewtype:
+view_type       = FilteredElementCollector(doc).OfClass(ViewFamilyType).WhereElementIsElementType().ToElements()
+view_type_plan  = {v_obj.FamilyName for v_obj in filter(lambda v_obj: v_obj.FamilyName in ('Floor Plan', 'Ceiling Plan'),view_type)}
+
+
+in4_room        = namedtuple("in4_room",["name","bbox","room_level_id","obj"])
 """--------------------------------------------------"""
-class NewView:
-    def __init__(self, name, bbox, level_id):
-        self.name = name
-        self.bbox = bbox
-        self.level_id = level_id
-def get_room_info():
-    for room in rooms:
-        room_name = room.LookupParameter("Name").AsString()
-        level = doc.GetElement(room.LevelId)
-        level_name = level.Name
-        room_type = doc.GetElement(room.GetTypeId())
-        room_type_name = room_type.Name
-        room_number = room.LookupParameter("Number").AsString()
-        room_occpancy=room.LookupParameter("Occupancy").AsString()
-        room_info_dict[room_name] = {
-            "Level": level_name,
-            "Number": room_number,
-            "Room Type": room_type_name,
-            "Occupancy": room_occpancy
-        }
-    return room_info_dict
 def offset_bbox(bbox, offset=1):
-    # if bbox:
-    #     try:
     bboxMinX = bbox.Min.X - offset
     bboxMinY = bbox.Min.Y - offset
     bboxMinZ = bbox.Min.Z - offset
@@ -76,137 +62,105 @@ def offset_bbox(bbox, offset=1):
     newBbox.Min = DB.XYZ(bboxMinX, bboxMinY, bboxMinZ)
     newBbox.Max = DB.XYZ(bboxMaxX, bboxMaxY, bboxMaxZ)
     return newBbox
-    #     except Exception as e:
-    #         print("{}".format(e))
-    #         raise e
-    # else:
-    #     TaskDialog.Show("Get Boundary","None Boundary was getted")
 
-def create_plan(new_view, view_type_id, cropbox_visible=True, remove_underlay=True):
+def create_plan(view, view_type_id, cropbox_visible=True):
     t = Transaction(doc, "Create Plan View")
     t.Start()
     try:
-        name = new_view.name
-        bbox = new_view.bbox
-        level_id = new_view.level_id
-        viewplan = DB.ViewPlan.Create(doc, view_type_id, level_id)
+        viewplan = DB.ViewPlan.Create(doc, view_type_id, view.room_level_id)
         viewplan.CropBoxActive = True
         viewplan.CropBoxVisible = cropbox_visible
-
-        if remove_underlay and revit.version.year == '2015':
-            underlay_param = viewplan.get_Parameter(DB.BuiltInParameter.VIEW_UNDERLAY_ID)
-            underlay_param.Set(DB.ElementId.InvalidElementId)
-
-        viewplan.CropBox = bbox
-
-        counter = 1
-        while True:
-            try:
-                viewplan.Name = name
-            except Exception:
-                try:
-                    viewplan.Name = '{} - Copy {}'.format(name, counter)
-                except Exception as e:
-                    counter += 1
-                    if counter > 100:
-                        raise Exception('Exceeded Maximum Loop')
-                else:
-                    break
-            else:
-                break
+        viewplan.CropBox = view.bbox
+        viewplan.Name = view.name
         t.Commit()
-
         return viewplan
     except Exception as e:
         print("{}".format(e))
         raise e
 """-----------------------------------"""
-selected_rooms = forms.SelectFromList.show(
-    room_type_sorted,
-    title="Select Room",
-    width=500,
-    button_name="Run",
-    multiselect=True)
-if selected_rooms:
-    for room in selected_rooms:
-        a=room.split(" |")
-        if "UNIT" in room:
-            list_rooms[a[1].strip(" #")] = a[-1]
-        else:
-            list_rooms[a[1].strip(" #")] = a[0]
-    print(list_rooms)
-    for room_number in list_rooms:
-        room_element = None
-        room_name = ""
-        for room in rooms:
-            if room.LookupParameter("Number").AsString() == room_number:
-                room_element = room
-                room_name = list_rooms[room_number]
-                break
-        if room_element:
-            if "UNIT" in room_element.LookupParameter("Name").AsString():
-                unittype=room_element.LookupParameter("Room Type").AsString()
-                name_planview = "ENLARGED PLAN - TYPE {} ({})".format(unittype,room_name)
-            else:
-                name_planview = "{} - ENLARGED PLAN - NUMBER {}".format(room_name,room_number)
-            room_level_id = room_element.Level.Id
-            room_bbox = room_element.get_BoundingBox(doc.ActiveView)
-            new_bbox = offset_bbox(room_bbox)
-            new_view = NewView(name=name_planview, bbox=new_bbox, level_id=room_level_id)
-            new_views.append(new_view)
-        else:
-            print("Room '{}' not found in the project.".format(room_name))
+#Show list of room to select
+selected_rooms = forms.SelectFromList.show( room_general,
+                                            title="Select Room",
+                                            width=500,
+                                            button_name="Run",
+                                            multiselect=True)
 
-    plan_type = forms.SelectFromList.show(
-        plan_types_options,
+#Check selected_rooms, if True -> continues, else -> raise
+if selected_rooms:
+    room_set={}
+    for room in selected_rooms:
+        a=room.split("|")
+        if "UNIT" in room:
+            room_set[re.sub(pattern, '',a[1])] = a[-1].strip(" ")
+        else:
+            room_set[re.sub(pattern, '',a[1])] = a[0].strip(" ")
+
+    #Get romm in4 to view
+    view_in4_list=[]
+    for room_number in room_set:
+        room_name = ""
+        for room in room_list:
+            if room.number == room_number:
+                if ("UNIT" in room.name) and (room.type is not None):
+                    name_planview = "ENLARGED PLAN - TYPE {} ({})".format(room.type,room.name)
+                else:
+                    name_planview = "ENLARGED PLAN - {} - (#{})".format(room.name,room_number)
+
+                room_level_id= room.obj.LevelId
+                room_bbox   = room.obj.get_BoundingBox(doc.ActiveView)
+                new_bbox    = offset_bbox(room_bbox)
+                view_plan   = in4_room(name_planview,new_bbox,room_level_id,room.obj)
+                view_in4_list.append(view_plan)
+    #Chose Plan Type
+    plan_types = forms.SelectFromList.show(
+        view_type_plan,
         title="Type View",
         width=500,
         button_name="Run",
         multiselect=True)
-    if plan_type:
-        if "Floor Plan" in plan_type:
+    if plan_types:
+        #chose Template
+        template_plan   = None
+        template_RCP    = None
+        if "Floor Plan" in plan_types:
             template_plan = forms.SelectFromList.show(
-                plan_templates,
+                sorted([v_obj.name for v_obj in view_temp_plan]),
                 title="View templates (Plan)",
                 width=500,
                 button_name="Run",
                 multiselect=False)
-
-        if "Ceiling Plan" in plan_type:
+        if "Ceiling Plan" in plan_types:
             template_RCP = forms.SelectFromList.show(
-                RCP_templates,
+                sorted([v_obj.name for v_obj in view_temp_RCP]),
                 title="View templates (RCP)",
                 width=500,
                 button_name="Run",
                 multiselect=False)
-        view_type_ids = {t.Id for t in plan_types if t.name in plan_type}
+
         """-----------------------------------"""
-        for view_type_id in view_type_ids:
-            for new_view in new_views:
-                view = create_plan(new_view= new_view, view_type_id=view_type_id)
+        for plan_type in plan_types:
+            for view in view_in4_list:
+                plan_type_id=next((i.Id for i in view_type if i.FamilyName == plan_type), None)
+                print(plan_type_id)
+                created_view = create_plan(view, plan_type_id)
                 if template_RCP or template_plan:
-                    if view.ViewType == ViewType.FloorPlan:
-                        template_name = template_plan
-                    elif view.ViewType == ViewType.CeilingPlan:
-                        template_name = template_RCP
-                    template_id = next((v.Id for v in viewlist if v.Name == template_name), None)
-
-                    t = Transaction(doc, "Assign Template")
-                    t.Start()
-
+                    template_id = None
                     try:
-                        if template_id:
-                            view.ViewTemplateId = template_id
-                        else:
-                            raise ValueError("No template found with name '{}'".format(template_name))
+                        if created_view.ViewType == ViewType.FloorPlan and template_plan:
+                            template_id     = next((v.obj.Id for v in view_temp_plan if v.obj.Name == template_plan), None)
+                        elif created_view.ViewType == ViewType.CeilingPlan and template_RCP:
+                            template_id     = next((v.obj.Id for v in view_temp_RCP if v.obj.Name == template_RCP), None)
+                    except:
+                        pass
+                    if template_id is not None:
+                        t = Transaction(doc, "Assign Template")
+                        t.Start()
+                        viewplan = doc.GetElement(created_view.Id)
+                        viewplan.ViewTemplateId = template_id
                         t.Commit()
-
-                    except Exception as e:
-                        t.RollBack()
-                        print("Error: {}".format(e))
+                    else:
+                        TaskDialog.Show("Create Plan", "Plan {} was created without a template".format(view.name))
                 else:
-                    TaskDialog.Show("Create Plan", "Plan {} was create without template".format(view.Name))
-    else:
-        TaskDialog.Show("Create Plan", "Please proceed")
+                    TaskDialog.Show("Create Plan", "Please proceed")
 else:
     TaskDialog.Show("Create Plan", "Please proceed")
